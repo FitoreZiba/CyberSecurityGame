fcb1df6d13769d589d6a5ca51a1f7eebfe15cecf
Fitore Ziba <zibafitore1@gmail.com>
2026-06-25 11:25:49 +0200

    Fixed WebSecurityConfig

--- backend/src/main/java/com/cybersecuritygame/backend/config/WebConfig.java
+++ backend/src/main/java/com/cybersecuritygame/backend/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
@@ -14,10 +13,13 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5175" +
-                                "http://localhost" +
-                                "http://localhost:80")
-                        .allowedMethods("*")
+                        .allowedOrigins(
+                                "http://localhost:5175",
+                                "http://localhost",
+                                "http://localhost:80"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
